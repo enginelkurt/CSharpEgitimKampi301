c9b2658956e08428ea5f95e7cbbfe41f9cb6c616
enginelkurt <elkurt_engin@hotmail.com>
2024-11-12 18:35:49 +0300

    Ders 12-Eklendi

--- CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -9,6 +9,15 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
     public class Order
     {
         public int OrderId { get; set; }
-      
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public List<Order> Orders { get; set; }
+        public int Quantity  { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int TotalPrice{ get; set; }
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
     }
 }
